41c26ce685d039b19e4363ac8155b46d1cdba7a0
Rob N ★ <robn@robn.io>
2017-02-20 21:50:02 +1100

    Initial loader and global detect

--- lib/RAID/Info.pm
+++ lib/RAID/Info.pm
@@ -1,5 +1,30 @@
+package RAID::Info;
+
 use strict;
 use warnings;
-package RAID::Info;
+
+my @drivers = qw(
+  Adaptec
+  Areca
+  MD
+  MegaRAID
+  SAS2IR
+  SAS3IR
+  SASMPT
+);
+
+sub detect {
+  my ($class) = @_;
+
+  my @controllers;
+
+  for my $driver (@drivers) {
+    my $package = "RAID::Info::Controller::$driver";
+    eval "use $package";
+    push @controllers, $package->detect;
+  }
+
+  return @controllers;
+}
 
 1;
