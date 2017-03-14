a9c037f87dcc18f94d918b13ae10420bfeb9283e
Rob N ★ <robn@robn.io>
2017-03-14 20:59:04 +1100

    Lift rebuild progress into role

--- lib/RAID/Info/VirtualDisk.pm
+++ lib/RAID/Info/VirtualDisk.pm
@@ -48,18 +48,7 @@ use namespace::autoclean;
 
 use Moo;
 with 'RAID::Info::VirtualDisk::State';
-
-use Scalar::Util qw(looks_like_number);
-
-has progress => (
-  is  => 'ro',
-  isa => Type::Tiny->new(
-    name       => 'Percent',
-    constraint => sub { looks_like_number($_) && $_ >= 0 && $_ <= 100 },
-    message    => sub { "$_ must be a number 0 <= n <= 100" },
-  ),
-  required => 1,
-);
+with 'RAID::Info::Disk::RebuildProgress';
 
 sub is_abnormal { 1 };
 }
